dbfa0743df76de4a9650f1e22aede012ded77945
Santinoco <santinot002@gmail.com>
2025-05-27 21:48:03 -0300

    Se agrego appointment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { AppointmentController } from './appointment/appointment.controller';
 
 @Module({
   imports: [DatabaseModule, UsersModule],
-  controllers: [AppController],
+  controllers: [AppController, AppointmentController],
   providers: [AppService],
 })
 export class AppModule {}
