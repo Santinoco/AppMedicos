26f284a05b615888e1e409f5d0038df1217e6eaa
m-doce <matias.serro@gmail.com>
2025-05-29 20:56:06 -0300

    Agregadas funciones de eliminar y actualizar ciudades

--- src/locations/locations.controller.ts
+++ src/locations/locations.controller.ts
@@ -5,6 +5,8 @@ import {
     Body,
     Param,
     ParseIntPipe,
+    Delete,
+    Patch
   } from "@nestjs/common";
   import { LocationService } from "./locations.service";
   
@@ -26,4 +28,20 @@ import {
     async createLocation(@Body() locationData: any) {
       return this.locationService.createLocation(locationData);
     }
+
+    @Delete(':location_id')
+    async deleteLocation(
+      @Param('location_id', ParseIntPipe) location_id: number
+    ) {
+      return this.locationService.deleteLocation(location_id);
+    }
+
+    @Patch(':location_id')
+    async updateLocation(
+      @Param('location_id', ParseIntPipe) location_id: number,
+      @Body() updateData: any,
+    ) {
+      return this.locationService.updateLocation(location_id, updateData);
+    }
+
   }
--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Location } from "./entities/location.model";
@@ -22,4 +22,20 @@ export class LocationService {
     const location = this.locationRepository.create(locationData);
     return this.locationRepository.save(location);
   }
+
+  async deleteLocation(location_id: number) {
+    const result = await this.locationRepository.delete({ location_id });
+    if (result.affected === 0) {
+      throw new NotFoundException(`Location with id ${location_id} not found`);
+    }
+    return { message: "Location deleted successfully" };
+  }
+
+  async updateLocation(location_id: number, updateData: any) {
+    const location = await this.locationRepository.findOne({ where: { location_id } });
+    if (!location) throw new NotFoundException('Location not found');
+    Object.assign(location, updateData);
+    return this.locationRepository.save(location);
+  }
+
 }
