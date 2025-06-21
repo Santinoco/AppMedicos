435e3b1d0fe62405f194dfb85768a3ba6bd612e5
diegui98 <diegui98@live.com.ar>
2025-06-20 23:04:53 -0300

    /admin la lista de usuarios ahora esta separada entre medicos y pacientes

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -5,55 +5,126 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { getUserId } from "../../services/userIdService";
 import axios from "axios";
-import { BackUser } from "../../types/backUser";
-import { Usuario } from "../../types/Usuario";
+import { BackMedico } from "../../types/backMedico";
+import { BackPaciente } from "../../types/backPaciente";
 
-const usuariosInicial: Usuario[] = [
+interface Medico {
+  especialidad: string;
+  matricula: string;
+  usuario: {
+    id: 0;
+    nombre: "";
+    apellido: "";
+    email: "";
+    activo: false;
+  };
+}
+
+interface Paciente {
+  consultasCompletadas: number;
+  seguroMedico: string;
+  usuario: {
+    id: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    activo: boolean;
+  };
+}
+
+const pacientesInicial: Paciente[] = [
   {
-    id: 0,
-    nombre: "",
-    apellido: "",
-    email: "",
-    activo: false,
-    tipo: 0, // 1 = admin, 2 = medico, 5 = paciente
+    consultasCompletadas: 0,
+    seguroMedico: "",
+    usuario: {
+      id: 0,
+      nombre: "",
+      apellido: "",
+      email: "",
+      activo: false,
+    },
+  },
+];
+
+const medicosInicial: Medico[] = [
+  {
+    especialidad: "",
+    matricula: "",
+    usuario: {
+      id: 0,
+      nombre: "",
+      apellido: "",
+      email: "",
+      activo: false,
+    },
   },
 ];
 
 export default function AdminDashboard() {
   const router = useRouter();
-  const [usuarios, setUsuarios] = useState<Usuario[]>(usuariosInicial);
+  const [medicos, setMedicos] = useState<Medico[]>(medicosInicial);
+  const [pacientes, setPacientes] = useState<Paciente[]>(pacientesInicial);
+  const [mostrarMedicos, setMostrarMedicos] = useState(true);
   const userId = getUserId();
 
   useEffect(() => {
     const fetchUsuarios = async () => {
       const token = localStorage.getItem("access_token");
       try {
-        const responseUsuarios = await axios.get(
-          `http://localhost:3000/users`,
+        const responseMedicos = await axios.get(
+          `http://localhost:3000/doctors`,
           {
             headers: { Authorization: `Bearer ${token}` },
           }
         );
-        const usuariosData: Usuario[] = responseUsuarios.data.map(
-          (usuario: BackUser) => ({
-            id: usuario.id,
-            nombre: usuario.nombre,
-            apellido: usuario.apellido,
-            email: usuario.email,
-            activo: usuario.activo,
-            tipo: usuario.user_type_id,
+        const medicosData: Medico[] = responseMedicos.data.map(
+          (medico: BackMedico) => ({
+            especialidad: medico.specialty,
+            matricula: medico.license_number,
+            usuario: {
+              id: medico.user.id,
+              nombre: medico.user.nombre,
+              apellido: medico.user.apellido,
+              email: medico.user.email,
+              activo: medico.user.activo,
+            },
           })
         );
 
-        setUsuarios(usuariosData);
+        setMedicos(medicosData);
       } catch (error) {
-        console.error("Error al obtener los usuarios:", error);
+        console.error("Error al obtener los medicos:", error);
+      }
+      try {
+        const responsePacientes = await axios.get(
+          `http://localhost:3000/patients`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        const pacientesData: Paciente[] = responsePacientes.data.map(
+          (paciente: BackPaciente) => ({
+            consultasCompletadas: paciente.completed_consultations,
+            seguroMedico: paciente.health_insurance,
+            usuario: {
+              id: paciente.user.id,
+              nombre: paciente.user.nombre,
+              apellido: paciente.user.apellido,
+              email: paciente.user.email,
+              activo: paciente.user.activo,
+            },
+          })
+        );
+
+        setPacientes(pacientesData);
+      } catch (error) {
+        console.error("Error al obtener los pacientes:", error);
       }
     };
     fetchUsuarios();
   }, [userId]);
 
-  const eliminarUsuario = async (idUsuario: number) => {
+  const eliminarUsuario = async (idUsuario: number, tipo: string) => {
     if (confirm("¿Estás seguro de que deseas eliminar este usuario?")) {
       const token = localStorage.getItem("access_token");
       // Realiza la solicitud DELETE al backend para eliminar el usuario
@@ -67,12 +138,23 @@ export default function AdminDashboard() {
         return;
       }
       // Modificacion visual de la lista de usuarios local
-      const nuevaListaUsuarios = usuarios.filter(
-        (usuario) => usuario.id !== idUsuario
-      );
-      setUsuarios(nuevaListaUsuarios);
+      if (tipo === "medico") {
+        const nuevaListaMedicos = medicos.filter(
+          (medico) => medico.usuario.id !== idUsuario
+        );
+        setMedicos(nuevaListaMedicos);
+      } else if (tipo === "paciente") {
+        const nuevaListaPacientes = pacientes.filter(
+          (paciente) => paciente.usuario.id !== idUsuario
+        );
+        setPacientes(nuevaListaPacientes);
+      }
       alert(`Usuario con ID ${idUsuario} eliminado.`);
     }
+  };
+
+  const toggleMedicos = () => {
+    setMostrarMedicos(!mostrarMedicos);
   };
 
   return (
@@ -92,62 +174,155 @@ export default function AdminDashboard() {
         </button>
       </div>
       <section className="bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-4">Usuarios registrados</h2>
-        {usuarios.length === 0 ? (
-          <p className="text-gray-500">No se encuentra ningun ususario.</p>
+        <div className="flex space-x-4 mb-4">
+          <button
+            className={`py-2 px-4 rounded ${
+              mostrarMedicos
+                ? "bg-green-600 text-white cursor-default"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+            disabled={mostrarMedicos}
+            onClick={() => setMostrarMedicos(true)}
+          >
+            Médicos
+          </button>
+          <button
+            className={`py-2 px-4 rounded ${
+              !mostrarMedicos
+                ? "bg-green-600 text-white cursor-default"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+            disabled={!mostrarMedicos}
+            onClick={() => setMostrarMedicos(false)}
+          >
+            Pacientes
+          </button>
+        </div>
+        <h2 className="text-xl font-semibold mb-4">
+          {mostrarMedicos ? "Médicos registrados" : "Pacientes registrados"}
+        </h2>
+        {mostrarMedicos ? (
+          <div>
+            {medicos.length === 0 ? (
+              <p className="text-gray-500">No se encuentra ningun medico.</p>
+            ) : (
+              <ul className="space-y-4">
+                {medicos.map((medico) => (
+                  <li
+                    className="border p-4 rounded-lg flex justify-between items-start"
+                    key={medico.usuario.id}
+                  >
+                    <div>
+                      <div>
+                        <span className="font-bold">
+                          {medico.usuario.nombre} {medico.usuario.apellido}
+                        </span>{" "}
+                        <span className="text-gray-500 font-light">
+                          - {medico.usuario.email}
+                        </span>
+                      </div>
+                      <div className="mb-1">
+                        <span className="">Especialidad: </span>{" "}
+                        {medico.especialidad}
+                      </div>
+                      <div className="mb-1">
+                        <span className=" mr-1">Matricula:</span>
+                        {medico.matricula}
+                      </div>
+                      <div className="mb-1">
+                        <span>
+                          {medico.usuario.activo ? (
+                            <strong className="text-green-600">Activo</strong>
+                          ) : (
+                            <strong className="text-red-700">Inactivo</strong>
+                          )}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex-col flex gap-4 items-center">
+                      <button
+                        onClick={() =>
+                          router.push("/admin/usuario/" + medico.usuario.id)
+                        }
+                        className="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition w-full"
+                      >
+                        Ver usuario
+                      </button>
+                      <button
+                        onClick={() =>
+                          eliminarUsuario(medico.usuario.id, "medico")
+                        }
+                        className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded transition w-full"
+                      >
+                        Eliminar Usuario
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         ) : (
-          <ul className="space-y-4">
-            {usuarios.map((usuario) => (
-              <li
-                className="border p-4 rounded-lg flex justify-between items-start"
-                key={usuario.id}
-              >
-                <div>
-                  <div>
-                    <span className="font-bold">
-                      {usuario.nombre} {usuario.apellido}
-                    </span>{" "}
-                    <span className="text-gray-500 font-light">
-                      - {usuario.email}
-                    </span>
-                  </div>
-                  <div className="mb-1">
-                    <span className="font-bold mr-2">Tipo de usuario:</span>
-                    {usuario.tipo === 2 ? (
-                      <span>Medico</span>
-                    ) : usuario.tipo === 5 ? (
-                      <span>Paciente</span>
-                    ) : (
-                      <span></span>
-                    )}
-                  </div>
-                  <div className="mb-1">
-                    <span>
-                      {usuario.activo ? (
-                        <strong className="text-green-600">Activo</strong>
-                      ) : (
-                        <strong className="text-red-700">Inactivo</strong>
-                      )}
-                    </span>
-                  </div>
-                </div>
-                <div className="flex-col flex gap-4 items-center">
-                  <button
-                    onClick={() => router.push("/admin/usuario/" + usuario.id)}
-                    className="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition w-full"
+          <div>
+            {pacientes.length === 0 ? (
+              <p className="text-gray-500">No se encuentra ningun paciente.</p>
+            ) : (
+              <ul className="space-y-4">
+                {pacientes.map((paciente) => (
+                  <li
+                    className="border p-4 rounded-lg flex justify-between items-start"
+                    key={paciente.usuario.id}
                   >
-                    Ver usuario
-                  </button>
-                  <button
-                    onClick={() => eliminarUsuario(usuario.id)}
-                    className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded transition w-full"
-                  >
-                    Eliminar Usuario
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+                    <div>
+                      <div>
+                        <span className="font-bold">
+                          {paciente.usuario.nombre} {paciente.usuario.apellido}
+                        </span>{" "}
+                        <span className="text-gray-500 font-light">
+                          - {paciente.usuario.email}
+                        </span>
+                      </div>
+                      <div className="mb-1">
+                        <span>Consultas completadas: </span>
+                        {paciente.consultasCompletadas}
+                      </div>
+                      <div className="mb-1">
+                        <span>Seguro médico: </span>
+                        {paciente.seguroMedico || "No especificado"}
+                      </div>
+                      <div className="mb-1">
+                        <span>
+                          {paciente.usuario.activo ? (
+                            <strong className="text-green-600">Activo</strong>
+                          ) : (
+                            <strong className="text-red-700">Inactivo</strong>
+                          )}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex-col flex gap-4 items-center">
+                      <button
+                        onClick={() =>
+                          router.push("/admin/usuario/" + paciente.usuario.id)
+                        }
+                        className="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition w-full"
+                      >
+                        Ver usuario
+                      </button>
+                      <button
+                        onClick={() =>
+                          eliminarUsuario(paciente.usuario.id, "paciente")
+                        }
+                        className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded transition w-full"
+                      >
+                        Eliminar Usuario
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         )}
       </section>
     </main>
