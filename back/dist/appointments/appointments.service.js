b8e7e5d0a6ab90f665c5f72654f8e29e7344cfb0
santinoco <santinot002@gmail.com>
2025-06-22 11:54:35 -0300

    Se agrega manejo de errores en Appointment

--- back/dist/appointments/appointments.service.js
+++ back/dist/appointments/appointments.service.js
@@ -27,120 +27,165 @@ let AppointmentsService = class AppointmentsService {
         this.calendarRepository = calendarRepository;
     }
     async getAllAppointments() {
-        return this.appointmentRepository.find({
-            relations: [
-                'patient_id',
-                'patient.user',
-                'doctor_id',
-                'doctor.user',
-                'status',
-                'slot_datetime',
-            ], order: { id: "ASC" }
-        });
+        try {
+            return this.appointmentRepository.find({
+                relations: [
+                    'patient_id',
+                    'patient.user',
+                    'doctor_id',
+                    'doctor.user',
+                    'status',
+                    'slot_datetime',
+                ], order: { id: "ASC" }
+            });
+        }
+        catch (error) {
+            throw new common_1.BadRequestException('Error al obtener los turnos.');
+        }
     }
     async getAppointmentById(id) {
-        return this.appointmentRepository.findOne({ relations: [
-                'patient_id',
-                'patient.user',
-                'doctor_id',
-                'doctor.user',
-                'status',
-                'slot_datetime',
-            ], where: { id } });
+        try {
+            return this.appointmentRepository.findOne({ relations: [
+                    'patient_id',
+                    'patient.user',
+                    'doctor_id',
+                    'doctor.user',
+                    'status',
+                    'slot_datetime',
+                ], where: { id } });
+        }
+        catch (error) {
+            throw new common_1.BadRequestException('Error al obtener el turno.');
+        }
     }
     async createAppointment(dto) {
-        const slot = await this.calendarRepository.findOne({ where: { slot_datetime: dto.slot_datetime } });
-        if (!slot) {
-            throw new common_1.NotFoundException("No existe un slot para esa fecha y hora.");
+        try {
+            const slot = await this.calendarRepository.findOne({ where: { slot_datetime: dto.slot_datetime } });
+            if (!slot) {
+                throw new common_1.NotFoundException("No existe un slot para esa fecha y hora.");
+            }
+            const appointment = this.appointmentRepository.create({
+                motivo: dto.motivo,
+                slot_datetime: slot,
+                doctor_id: dto.doctor_id,
+                patient_id: dto.patient_id,
+            });
+            return await this.appointmentRepository.save(appointment);
         }
-        const appointment = this.appointmentRepository.create({
-            motivo: dto.motivo,
-            slot_datetime: slot,
-            doctor_id: dto.doctor_id,
-            patient_id: dto.patient_id,
-        });
-        return await this.appointmentRepository.save(appointment);
+        catch (error) {
+            throw new common_1.BadRequestException('Error al crear el turno.');
+        }
     }
     async updateAppointmentStatus(id, newStatus) {
-        await this.appointmentRepository.update(id, { estado_id: newStatus });
-        return this.getAppointmentById(id);
+        try {
+            await this.appointmentRepository.update(id, { estado_id: newStatus });
+            return this.getAppointmentById(id);
+        }
+        catch (error) {
+            throw new common_1.BadRequestException('Error al actualizar el estado del turno.');
+        }
     }
     async findByDoctorId(doctorUserId) {
-        return this.appointmentRepository.find({
-            where: {
-                doctor: { user_id: doctorUserId },
-            },
-            relations: [
-                'doctor', 'doctor.user',
-                'patient', 'patient.user',
-                'status',
-            ],
-            order: {
-                id: 'ASC',
-            },
-        });
+        try {
+            return this.appointmentRepository.find({
+                where: {
+                    doctor: { user_id: doctorUserId },
+                },
+                relations: [
+                    'doctor', 'doctor.user',
+                    'patient', 'patient.user',
+                    'status',
+                ],
+                order: {
+                    id: 'ASC',
+                },
+            });
+        }
+        catch (error) {
+            throw new common_1.BadRequestException('Error al obtener los turnos del doctor.');
+        }
     }
     async findByPatientId(patientUserId) {
-        return this.appointmentRepository.find({
-            where: {
-                patient: { user_id: patientUserId },
-            },
-            relations: [
-                'doctor', 'doctor.user',
-                'patient', 'patient.user',
-                'status',
-            ],
-            order: {
-                id: 'ASC',
-            },
-        });
+        try {
+            return this.appointmentRepository.find({
+                where: {
+                    patient: { user_id: patientUserId },
+                },
+                relations: [
+                    'doctor', 'doctor.user',
+                    'patient', 'patient.user',
+                    'status',
+                ],
+                order: {
+                    id: 'ASC',
+                },
+            });
+        }
+        catch (error) {
+            throw new common_1.BadRequestException('Error al obtener los turnos del paciente.');
+        }
     }
     async deleteAppointment(id) {
-        const appointment = await this.appointmentRepository.findOne({ where: { id } });
-        if (!appointment) {
-            throw new common_1.NotFoundException(`Cita con ID ${id} no encontrada`);
+        try {
+            const appointment = await this.appointmentRepository.findOne({ where: { id } });
+            if (!appointment) {
+                throw new common_1.NotFoundException(`Cita con ID ${id} no encontrada`);
+            }
+            await this.appointmentRepository.remove(appointment);
+            return "Turno eliminado correctamente";
         }
-        await this.appointmentRepository.remove(appointment);
-        return "Turno eliminado correctamente";
+        catch (error) {
+            throw new common_1.BadRequestException('Error al eliminar el turno.');
+        }
     }
     async getAppointmentsByPatientName(name) {
-        const appointments = await this.appointmentRepository.find({
-            where: {
-                patient: { user: { nombre: name } },
-            },
-            relations: [
-                'doctor', 'doctor.user',
-                'patient', 'patient.user',
-                'status',
-            ],
-            order: {
-                id: 'ASC',
-            },
-        });
-        return appointments;
+        try {
+            const appointments = await this.appointmentRepository.find({
+                where: {
+                    patient: { user: { nombre: name } },
+                },
+                relations: [
+                    'doctor', 'doctor.user',
+                    'patient', 'patient.user',
+                    'status',
+                ],
+                order: {
+                    id: 'ASC',
+                },
+            });
+            return appointments;
+        }
+        catch (error) {
+            throw new common_1.BadRequestException('Error al obtener los turnos por nombre de paciente.');
+        }
     }
     async getAppointmentsByDate(date) {
-        const input = typeof date === 'string' ? new Date(date + 'T00:00:00') : new Date(date);
-        const start = new Date(input);
-        start.setHours(0, 0, 0, 0);
-        const end = new Date(input);
-        end.setHours(23, 59, 59, 999);
-        const appointments = await this.appointmentRepository.find({
-            where: {
-                slot_datetime: {
-                    slot_datetime: (0, typeorm_3.Between)(start, end),
+        try {
+            const input = typeof date === 'string' ? new Date(date + 'T00:00:00') : new Date(date);
+            const start = new Date(input);
+            start.setHours(0, 0, 0, 0);
+            const end = new Date(input);
+            end.setHours(23, 59, 59, 999);
+            const appointments = await this.appointmentRepository.find({
+                where: {
+                    slot_datetime: {
+                        slot_datetime: (0, typeorm_3.Between)(start, end),
+                    },
                 },
-            },
-            relations: [
-                'doctor', 'doctor.user',
-                'patient', 'patient.user',
-                'status', 'slot_datetime'
-            ],
-            order: {
-                id: 'ASC',
-            }
-        });
-        return appointments;
+                relations: [
+                    'doctor', 'doctor.user',
+                    'patient', 'patient.user',
+                    'status', 'slot_datetime'
+                ],
+                order: {
+                    id: 'ASC',
+                }
+            });
+            return appointments;
+        }
+        catch (error) {
+            throw new common_1.BadRequestException('Error al obtener los turnos por fecha.');
+        }
     }
 };
 exports.AppointmentsService = AppointmentsService;
