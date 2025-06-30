41e2a38fdff9b4ce28ecd9da9d925bd33cab4d38
Valentina <vsosa@fi.uba.ar>
2025-06-30 20:34:56 -0300

    agrego mas botones de volver en /paciente

--- frontend/src/app/paciente/mis-turnos/page.tsx
+++ frontend/src/app/paciente/mis-turnos/page.tsx
@@ -6,6 +6,23 @@ export default function MisTurnosInicio() {
 
   return (
     <div className="p-10 max-w-3xl mx-auto">
+      {/* Botón Volver */}
+      <button
+        onClick={() => router.push('/paciente')}
+        className="absolute top-9 left-80 flex items-center gap-2 text-green-600 hover:text-green-800 transition"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+        </svg>
+        Volver a inicio
+      </button>
       <h1 className="text-3xl font-bold text-green-800 mb-6">Gestión de turnos</h1>
 
       <div className="flex flex-col gap-4">
